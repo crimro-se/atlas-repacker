771a1df4cf31e84aed87cafe1f6a2901c158a7ba
crimro.se <crimrosegit@proton.me>
2024-10-26 05:15:21 +0100

    Basic unit tests for box packing and detecting.

--- internal/boxpack/boxes_test.go
+++ internal/boxpack/boxes_test.go
@@ -1,16 +1,50 @@
 package boxpack
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/base64"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/png"
-	"os"
 	"testing"
 )
 
-func TestPack(t *testing.T) {
+var testimg_b64 string = `
+iVBORw0KGgoAAAANSUhEUgAAAQAAAACAAgMAAACZ21+ZAAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9
+kT1Iw0AcxV9TpVIqDnbQ4pChOtlFRQSXWoUiVAi1QqsOJpd+QZOGpMXFUXAtOPixWHVwcdbVwVUQ
+BD9AnB2cFF2kxP8lhRYxHhz34929x907QGhWmGb1xAFNr5npZELM5lbFwCuCiEDALIZlZhlzkpSC
+5/i6h4+vdzGe5X3uz9Gv5i0G+ETiODPMGvEG8fRmzeC8TxxmJVklPiceN+mCxI9cV1x+41x0WOCZ
+YTOTnicOE4vFLla6mJVMjXiKOKpqOuULWZdVzluctUqdte/JXxjK6yvLXKc5giQWsQQJIhTUUUYF
+NcRo1UmxkKb9hIc/4vglcinkKoORYwFVaJAdP/gf/O7WKkxOuEmhBND7Ytsfo0BgF2g1bPv72LZb
+J4D/GbjSO/5qE5j5JL3R0aJHwMA2cHHd0ZQ94HIHGHoyZFN2JD9NoVAA3s/om3LA4C0QXHN7a+/j
+9AHIUFepG+DgEBgrUva6x7v7unv790y7vx/Op3LLMNEO0gAAAAlwSFlzAAAuIwAALiMBeKU/dgAA
+AAd0SU1FB+gKGgMNCY3j5KUAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAA
+CVBMVEUAAAAAAAD///+D3c/SAAAAAXRSTlMAQObYZgAAAAFiS0dEAmYLfGQAAABfSURBVGiB7c3B
+CcAgEEVBm7AvD27/rQQ8hRBiDhLUzDsun9mUpEHlaFUAAPAIRPQeALYAOv0NiEsVAFgJyG1QABMD
+LwIAAB8C52uZD0i3MwAAAAAAAAAAoB9gACBp+w67yiWwcnXERQAAAABJRU5ErkJggg==`
+
+func loadTestImg() (image.Image, error) {
+	pngBytes, err := base64.StdEncoding.DecodeString(testimg_b64)
+	if err != nil {
+		return nil, err
+	}
+	img, err := png.Decode(bytes.NewReader(pngBytes))
+	return img, err
+}
+
+func TestIslands(t *testing.T) {
+	img, err := loadTestImg()
+	if err != nil {
+		t.Error(err)
+	}
+	boxes := ImageToBoxes(img, false)
+	if len(boxes) != 5 {
+		t.Fail()
+	}
+}
+
+func TestPacking(t *testing.T) {
 	var boxes []Box
 	boxes = append(boxes, Box{sourceRect: image.Rect(0, 0, 10, 10)})
 	boxes = append(boxes, Box{sourceRect: image.Rect(0, 0, 10, 10)})
@@ -29,24 +63,39 @@ func TestPack(t *testing.T) {
 	boxes = append(boxes, Box{sourceRect: image.Rect(0, 0, 10, 20)})
 	boxes = append(boxes, Box{sourceRect: image.Rect(0, 0, 30, 30)})
 	boxes = append(boxes, Box{sourceRect: image.Rect(0, 0, 50, 50)})
-	//fmt.Println(PackBoxes(boxes, 100, 40, 1, 0))
-	ab, _ := PackAllBoxes(boxes, 100, 40, 1, 0)
-
-	img := DrawRects(ab[0], 100, 40)
-
-	// test ability to find box in img
-	fmt.Println(ImageToBoxes(img, false))
-
-	file, err := os.Create("test.png")
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+	unpacked := PackBoxes(boxes, 900, 40, 1, 0)
+	if unpacked != 1 {
+		t.Fail()
 	}
-	defer file.Close()
-	png.Encode(file, img)
+	unpacked = PackBoxes(boxes, 900, 51, 1, 0)
+	if unpacked != 0 {
+		t.Fail()
+	}
+
+	// verify exactly one box occupies 0,0
+	c := 0
+	for _, box := range boxes {
+		if box.destRect.Min.X == 0 && box.destRect.Min.Y == 0 {
+			c++
+		}
+	}
+	if c != 1 {
+		t.Fail()
+	}
+
+	// verify src boxes still all at 0,0
+	c = 0
+	for _, box := range boxes {
+		if box.sourceRect.Min.X == 0 && box.sourceRect.Min.Y == 0 {
+			c++
+		}
+	}
+	if c != len(boxes) {
+		t.Fail()
+	}
 }
 
-func DrawRects(boxes []Box, W, H int) image.Image {
+func drawRects(boxes []Box, W, H int) image.Image {
 	img := image.NewRGBA64(image.Rect(0, 0, W, H))
 	src := image.NewUniform(color.RGBA{0, 100, 255, 255})
 	for _, b := range boxes {
